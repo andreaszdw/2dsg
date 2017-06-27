7e420ebd371cd66c944b7f0c7e2b2e8500adc865
andreaszdw <andreaszdw@googlemail.com>
2017-06-27 11:12:39 +0200

    Insert fps logging

--- 2dSpaceGame/src/de/keinpunkt/Game.java
+++ 2dSpaceGame/src/de/keinpunkt/Game.java
@@ -1,13 +1,25 @@
 package de.keinpunkt;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.FPSLogger;
 
 public class Game implements ApplicationListener {
+	
+	private SpriteBatch batch;
+	private BitmapFont font;
+	private FPSLogger fpslogger;
 
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		font.setColor(Color.RED);
+		fpslogger = new FPSLogger();
 	}
 
 	@Override
@@ -18,8 +30,14 @@ public class Game implements ApplicationListener {
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		font.draw(batch, "Hello World", 10, 10);
+		batch.end();
 
+		fpslogger.log();
 	}
 
 	@Override
@@ -36,8 +54,8 @@ public class Game implements ApplicationListener {
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
+		batch.dispose();
+		font.dispose();
 	}
 
 }
