e1ffad4da9f07d0a225c1171df3e7093c62e2178
andreaszdw <andreaszdw@googlemail.com>
2017-06-28 15:17:50 +0200

    Test Screen

---
+++ 2dSpaceGame/src/de/keinpunkt/OptionsScreen.java
@@ -0,0 +1,64 @@
+package de.keinpunkt;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class OptionsScreen implements Screen {
+        final GameMain game;
+        
+	OrthographicCamera camera;
+
+	public OptionsScreen(final GameMain gam) {
+		game = gam;
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		game.batch.setProjectionMatrix(camera.combined);
+
+		game.batch.begin();
+		game.font.draw(game.batch, "Option Screen!", 100, 150);
+		game.batch.end();
+
+		if (Gdx.input.isTouched()) {
+			game.setScreen(new GameScreen(game));
+			dispose();
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		camera.setToOrtho(false, width, height);
+		System.out.println(Gdx.graphics.getWidth() + " " + Gdx.graphics.getHeight());
+	}	
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+}
